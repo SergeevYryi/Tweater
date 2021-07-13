f91351f0301510e92202ab0bbcfe18f36d3e6074
SergeevYryi <sergeev.y90@inbox.ru>
2021-07-13 20:25:40 +0400

    part 1.3

--- src/main/java/com/applic/domain/Message.java
+++ src/main/java/com/applic/domain/Message.java
@@ -13,20 +13,27 @@ public class Message {
     private String text;
     private String tag;
 
+    public Message() {
+
+    }
+    public Message(String text, String tag) {
+        this.text = text;
+        this.tag = tag;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+    public String getText() {
+        return text;
+    }
+
     public Integer getId() {
         return id;
     }
 
     public void setId(Integer id) {
         this.id = id;
-    }
-
-    public String getText() {
-        return text;
-    }
-
-    public void setText(String text) {
-        this.text = text;
     }
 
     public String getTag() {
--- src/main/java/com/applic/repos/MessageRepo.java
+++ src/main/java/com/applic/repos/MessageRepo.java
@@ -1,4 +1,10 @@
 package com.applic.repos;
 
-public interface MessageRepo {
+import com.applic.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepo extends CrudRepository<Message, Long> {
+    List<Message> findByTag(String tag);
 }
